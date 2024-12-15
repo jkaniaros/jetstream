20a6a2abd77a8abfd8c7a06ec0f7f3e6a98aaee7
Jannis Kaniaros <kanjannis@gmail.com>
2024-12-15 17:46:10 +0100

    Fixed Kafka Consumer, added wait and error handling

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaConsumer, KafkaClient
+from kafka.errors import NoBrokersAvailable
 from concurrent.futures import ThreadPoolExecutor
 import os
 import time
@@ -6,8 +7,16 @@ import time
 KAFKA_BROKER = os.environ.get("KAFKA_BROKER")
 print(f"Kafka Broker: {KAFKA_BROKER}")
 
+WAIT_SEC = 1
+
 def consume_messages(topic):
-    print(f"Consuming topic {topic.list()}")
+    """
+    Connect to a specified Kafka topic and consume messages from it
+    
+    Parameters:
+    topic (str): The Kafka topic to consume messages from.
+    """
+    print(f"Consuming topic {topic}")
     consumer = KafkaConsumer(
         topic,
         bootstrap_servers=KAFKA_BROKER,
@@ -25,27 +34,50 @@ def consume_messages(topic):
     
 
 def run():
+    """
+    Entrypoint of the consumer. Reads all topics within the Kafka broker
+    """
     print("Run")
-    kafka_client = KafkaClient(bootstrap_servers=KAFKA_BROKER, client_id="data-consumer")
-    future = kafka_client.cluster.request_update()
-    kafka_client.poll(future=future)
-    topics = kafka_client.cluster.topics()
+    topics = []
+    try:
+        kafka_client = KafkaClient(bootstrap_servers=KAFKA_BROKER, client_id="data-consumer")
+        future = kafka_client.cluster.request_update()
+        kafka_client.poll(future=future)
+        topics = kafka_client.cluster.topics()
+        
+        print(f"Topics: {list(topics)}")
+    except NoBrokersAvailable:
+        print(f"No Kafka broker available... Retrying in {WAIT_SEC} secons")
 
-    print(f"Topics: {list(topics)}")
     # Create a ThreadPoolExecutor to process each topic in parallel
     if topics:
-        with ThreadPoolExecutor(max_workers=len(topics)) as executor:
-            executor.map(consume_messages, topics)
-            # TODO: await execution - currently immediate return!!
+        # Use 75% of the available CPU cores
+        with ThreadPoolExecutor(max_workers=int(os.cpu_count()*0.75)) as executor:
+            # Create a consume_messages task for each topic and submit it to the executor
+            # Each topic is processed concurrently in its own thread by the consume_messages function
+            futures = {executor.submit(consume_messages, topic): topic for topic in topics}
+
+            # Await every future
+            for f in futures:
+                try:
+                    f.result()
+                except Exception as e:
+                    print(f"Error in thread for topic {futures[future]}: {e}")
     else:
         print(f"{time.ctime()} No topics")
-        time.sleep(5)
+        
+        time.sleep(WAIT_SEC)
+        
+        # Double the waiting time up to 512 sec (8.5 min)
+        if WAIT_SEC <= 256:
+            WAIT_SEC *= 2
 
 if __name__ == "__main__":
     print("Starting...")
-    #try:
-        #while True:
-    run()
+    
+    try:
+        while True:
+            run()
 
-    #except KeyboardInterrupt:
-    #   print("Exit")
+    except KeyboardInterrupt:
+        print("Exit")
