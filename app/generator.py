8417beef008e01f5f97c3598ad5e156327d18475
Jannis Kaniaros <kanjannis@gmail.com>
2024-12-27 14:09:49 +0100

    Endless loop in generator

--- app/generator.py
+++ app/generator.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import time
 from helper.file_downloader import download_all_files
 from helper.zip_extractor import extract_all_product_txt_files
 from helper.kafka_publisher import publish_staged, create_topic, publish_file
@@ -24,36 +25,47 @@ DESCRIPTION_FILE = "zehn_now_ff_Beschreibung_Stationen.txt"
 KAFKA_BROKER = os.environ.get("KAFKA_BROKER")
 print(f"Kafka Broker: {KAFKA_BROKER}")
 
+LOOP_WAIT_TIME = 10  # Time to wait between iterations (in seconds)
+
 def run():
     """
-    Data generation entrypoint. Downloads all files from the file server and extract all zip files to `tmp/staged`
+    Endless loop entrypoint. Downloads all files from the file server, extracts relevant txt files,
+    processes data, and publishes to Kafka. Loops indefinitely with a waiting time in between the iterations.
     """
-    ### shutil.rmtree("tmp")
+    max_run_ts = None  # Start with no timestamp
 
     # Create topics if they don't exist, so that the spark applications don't crash because of missing topics
     create_topic(KAFKA_TOPIC, KAFKA_BROKER)
     create_topic(KAFKA_DESCRIPTION_TOPIC, KAFKA_BROKER)
 
-    # Download zip files from DWD and extract the relevant txt files
-    download_all_files(DOWNLOAD_URL, DOWNLOAD_FOLDER)
-    extract_all_product_txt_files(DOWNLOAD_FOLDER, EXTRACT_REGEX, STAGED_FOLDER)
 
-    # If a station description file exists, copy it to the staged folder too
-    if os.path.exists(os.path.join(DOWNLOAD_FOLDER, DESCRIPTION_FILE)):
-        shutil.copy2(os.path.join(DOWNLOAD_FOLDER, DESCRIPTION_FILE), STAGED_FOLDER)
+    while True:
+        print("Stating new iteration...")
+        
+        # Download zip files from DWD and extract the relevant txt files
+        _, max_run_ts = download_all_files(DOWNLOAD_URL, DOWNLOAD_FOLDER, max_run_ts)
+        extract_all_product_txt_files(DOWNLOAD_FOLDER, EXTRACT_REGEX, STAGED_FOLDER)
+
+        # If a station description file exists, copy it to the staged folder too
+        if os.path.exists(os.path.join(DOWNLOAD_FOLDER, DESCRIPTION_FILE)):
+            shutil.copy2(os.path.join(DOWNLOAD_FOLDER, DESCRIPTION_FILE), STAGED_FOLDER)
 
 
-    if os.path.exists(os.path.join(STAGED_FOLDER, DESCRIPTION_FILE)):
-        publish_file(os.path.join(STAGED_FOLDER, DESCRIPTION_FILE),
-                     KAFKA_BROKER, KAFKA_DESCRIPTION_TOPIC)
+        if os.path.exists(os.path.join(STAGED_FOLDER, DESCRIPTION_FILE)):
+            publish_file(os.path.join(STAGED_FOLDER, DESCRIPTION_FILE),
+                        KAFKA_BROKER, KAFKA_DESCRIPTION_TOPIC)
 
-    # Publish all extracted files to Kafka
-    publish_staged(STAGED_FOLDER, KAFKA_BROKER, KAFKA_TOPIC, TOPIC_KEY_REGEX)
+        # Publish all extracted files to Kafka
+        publish_staged(STAGED_FOLDER, KAFKA_BROKER, KAFKA_TOPIC, TOPIC_KEY_REGEX)
 
-    # Remove the temporary zip download folder
-    if os.path.exists(DOWNLOAD_FOLDER):
-        shutil.rmtree(DOWNLOAD_FOLDER)
+        # Remove the temporary folder
+        if os.path.exists("./tmp"):
+            shutil.rmtree("./tmp")
 
+        print(f"Finishing iteration, waiting {LOOP_WAIT_TIME} seconds")
+        
+        # Wait for a defined interval before the next run
+        time.sleep(LOOP_WAIT_TIME)
 
 if __name__ == "__main__":
     run()
--- app/helper/file_downloader.py
+++ app/helper/file_downloader.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import requests
 from urllib.parse import urljoin
 from html.parser import HTMLParser
@@ -6,19 +7,36 @@ from html.parser import HTMLParser
 class HtmlParser(HTMLParser):
     """
     HTML Parser that handles link entries in the HTML.
-    Saves the URLs in the field `file_links`
+    Saves the URLs in the field `file_links` and tracks the latest update timestamp.
     """
     def __init__(self, base_url):
         super().__init__()
         self.base_url = base_url
-        self.file_links = []
+        self.file_links = [] # To store all links
+        self.latest_timestamp = None  # To store the maximum timestamp
 
     def handle_starttag(self, tag, attrs):
+        # Check for <a> tag (links)
         if tag == 'a':
             for attr in attrs:
                 if attr[0] == 'href':
                     file_url = urljoin(self.base_url, attr[1])
                     self.file_links.append(file_url)
+
+
+    def handle_data(self, data):
+        # Check if the data is a timestamp
+        data = data.strip()
+        try:
+            # Attempt to parse the first 16 characters of data as a timestamp. Data contains of TS + Size
+            timestamp = datetime.strptime(data[:17], "%d-%b-%Y %H:%M")
+            # Update the maximum timestamp
+            if self.latest_timestamp is None or timestamp > self.latest_timestamp:
+                self.latest_timestamp = timestamp
+        except ValueError:
+            # Not a valid timestamp, so skip
+            pass
+
 
 
 def download_file(url: str, folder: str):
@@ -42,13 +60,18 @@ def download_file(url: str, folder: str):
     else:
         print(f"Failed to download {filename}")
 
-def download_all_files(url: str, folder: str):
+def download_all_files(url: str, folder: str, min_timestamp: datetime) -> (int, datetime):
     """
-    Function to download all file found in a specified URL and save them to a folder
+    Function to download all file found in a specified URL and save them to a folder. Download is only executed, if the maximum timestamp on the page is greater than the provided one.
 
     Parameters:
     url (str): The URL of the files to download. The files should be found as HTML links.
     folder (str): The folder where the files should be saved.
+    min_timestamp (datetime): The minimum required datetime to download files.
+    
+    Returns:
+    result (int): 0 if ok, -1 if access failed, 1 if no download was executed because of timestamp comparison
+    timestamp (datetime): The latest timestamp found on the URL.
     """
 
     # Create download folder if it doesn't exist
@@ -61,9 +84,16 @@ def download_all_files(url: str, folder: str):
         parser = HtmlParser(url)
         parser.feed(r.text)
 
-        for file_url in parser.file_links:
-            if file_url.startswith(url):
-                print(f"Downloading {file_url}")
-                download_file(file_url, folder)
+        print(f"Newest update timestamp from url: {parser.latest_timestamp} - given timestamp: {min_timestamp}")
+        if min_timestamp is None or parser.latest_timestamp > min_timestamp:
+            for file_url in parser.file_links:
+                if file_url.startswith(url):
+                    print(f"Downloading {file_url}")
+                    download_file(file_url, folder)
+            return (0, parser.latest_timestamp)
+        
+        # If no download was necessary
+        return (1, parser.latest_timestamp)
     else:
         print(f"Failed to access {url}")
+        return (-1, "")
